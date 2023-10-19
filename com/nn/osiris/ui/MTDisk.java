f0d6cc43e7f94a84e0bd484089d419587ac39927
Dale Sinder <sinder@illinois.edu>
2023-10-18 21:27:03 -0500

    Restore default drive 0 iff user did not provide one before a boot attempt

--- com/nn/osiris/ui/MTDisk.java
+++ com/nn/osiris/ui/MTDisk.java
@@ -43,7 +43,7 @@ public class MTDisk {
 			file = null;
 			
 			JOptionPane.showMessageDialog(PortalFrame.getFrames()[0],
-					"<html>Unable to open MTutor disk:  " + fn +  "</html>",
+					"<html>Unable to open MTutor disk:  " + fn +  " Already Open?</html>",
 					"File Error",
 					JOptionPane.WARNING_MESSAGE);
 			
--- com/nn/osiris/ui/PortalConsts.java
+++ com/nn/osiris/ui/PortalConsts.java
@@ -47,7 +47,7 @@ class PortalConsts
 	
 //	static final String BootDisk = "drsutil.mte";  // no longer used
 
-//	static final String InitDisk0 = "drsutil.mte";
+	static final String InitDisk0 = "drsutil.mte";
 //	static final String InitDisk1 = "ptermhelp.mte";
 	
 	
